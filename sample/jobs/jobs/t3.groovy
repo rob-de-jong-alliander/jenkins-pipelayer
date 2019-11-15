3dcbe40509d03d903c0d00f863ae064e7da2e247
Jong <rob.de.jong@alliander.com>
2019-11-15 12:11:14 +0100

    gitlabhook test

---
+++ sample/jobs/jobs/t3.groovy
@@ -0,0 +1,16 @@
+job('example') {
+    triggers {
+        gitlabPush {
+            buildOnMergeRequestEvents(false)
+            buildOnPushEvents(false)
+            enableCiSkip(false)
+            setBuildDescription(false)
+            rebuildOpenMergeRequest('never')
+            includeBranches('include1,include2')
+            excludeBranches('exclude1,exclude2')
+        }
+    }
+    steps{
+        sh 'echo test'
+    }
+}
