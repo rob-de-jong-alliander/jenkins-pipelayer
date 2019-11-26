f69eda8604db4da22536f58021fbf33b208434fa
Jong <rob.de.jong@alliander.com>
2019-11-26 13:53:19 +0100

    t3 simple

--- sample/jobs/jobs/t3.groovy
+++ sample/jobs/jobs/t3.groovy
@@ -1,25 +1,5 @@
 #!/usr/bin/env groovy
-
-freeStyleJob('example') {
-    logRotator(-1, 10)
-    scm {
-        github('jenkinsci/job-dsl-plugin', 'master')
-    }
-    triggers {
-        gitlabPush {
-            buildOnMergeRequestEvents(false)
-            buildOnPushEvents(false)
-            enableCiSkip(false)
-            setBuildDescription(false)
-            rebuildOpenMergeRequest('never')
-            includeBranches('include1,include2')
-            excludeBranches('exclude1,exclude2')
-        }
-    }
-    steps {
-        gradle('clean build')
-    }
-    publishers {
-        archiveArtifacts('job-dsl-plugin/build/libs/job-dsl.hpi')
-    }
+def myJob = freeStyleJob('SimpleJob')
+myJob.with {
+    description 'A Simple Job'
 }
